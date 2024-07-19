cb7b809a24fa00fc6cb2099c7e4499a0674ad17a
KSnie <u6510348@au.edu>
2024-07-19 15:32:52 +0700

    Complete Accessory-shop

--- accessory-shop/src/components/Footer.jsx
+++ accessory-shop/src/components/Footer.jsx
@@ -4,8 +4,8 @@ const Footer = () => {
     <footer className="text-center bg-body-tertiary mt-5">
 
       <div className="text-center p-3" style={{ backgroundColor: 'rgba(0, 0, 0, 0.05)', marginTop: '16.5%'}}>
-        © 2024 Copyright :
-        <a className="text-body" href="#NNNDev">NNNDev</a>
+        Web-Develop Class :
+        <a className="text-body" href="#NNNDev">Kasidis Chuayprasert</a>
       </div>
     </footer>
   );
